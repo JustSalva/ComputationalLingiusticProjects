77b4b7ee774792d06f1192bb697bf5d5d2363cb5
Matteo Salvadore <matteo.salvadore@gmail.com>
2018-10-11 16:59:24 +0200

    re fixes are over 9000 - semicit

--- regularExpressions.py
+++ regularExpressions.py
@@ -101,7 +101,7 @@ class regularExpressions:
         self.reList.append(('SLASH', re.compile('/')))
         self.reList.append(('DURATION',
                             re.compile('(([1-9][0-9]{3}-[1-9][0-9]{3})|((' + constants.NUMBERS_IN_LETTER + '|'
-                                       + '[1-9][0-9]*)-(' + __STRING_TIMEUNIT_SINGULAR + '))|' + __STRING_TIME
+                                       + '[1-9][0-9]*)-(' + __STRING_TIMEUNIT_SINGULAR + '|hour))|' + __STRING_TIME
                                        + '-' + __STRING_TIME + 'hrs)', flags=re.IGNORECASE)))
         self.reList.append(('THE', re.compile('the')))
         pass
@@ -136,6 +136,6 @@ for elem in re.finditer(re.compile('(([1-9][0-9]{3}-[1-9][0-9]{3})|((' + constan
     print elem.group(), elem.span()
 """
 a = regularExpressions()
-print (regularExpressions.checkRE(a, "three-year"))
+print (regularExpressions.checkRE(a, "eight-hour"))
 
 
