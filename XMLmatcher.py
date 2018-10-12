e10bf5a7d2359bd6dda95afa448ae930092071f1
Unknown <fabiomercurio1995@gmail.com>
2018-10-12 20:25:53 +0200

    Temporal update relaxed counter, not working

--- XMLmatcher.py
+++ XMLmatcher.py
@@ -41,8 +41,18 @@ def list_compare_relaxed(list1,list2):
     RICORDATI DI FARE LA CORREZIONE PER CUI DEVI PRIMA SCEGLIERE IL MATCH PIU' GRANDE!!!!!!!!!!!
     '''
     for num1 in range(len(list1)):
+        begin_1 = int(list1[num1][2])
+        end_1 = int(list1[num1][3])
         for num2 in range(len(list2)):
-            if list1[num1][1]==list2[num2][1] and ((list1[num1][0] in list2[num2][0]) or (list2[num2][0] in list1[num1][0])) and num1 not in to_not_be_seen_postitions_list1 and num2 not in to_not_be_seen_postitions_list2:
+            begin_2 = int(list2[num2][2])
+            end_2 = int(list2[num2][3])
+
+            overlap = (begin_1<=begin_2 and end_1>=end_2) or (begin_2<=begin_1 and end_2>=end_1) or \
+                      (begin_1<=begin_2<=end_1) or (begin_2<=begin_1<=end_2) or \
+                      (begin_1<=end_2<=end_1) or (begin_2<=end_1<=end_2)
+
+
+            if list1[num1][1]==list2[num2][1] and overlap:
                 cnt = cnt + 1
                 to_not_be_seen_postitions_list1.append(num1)
                 to_not_be_seen_postitions_list2.append(num2)
