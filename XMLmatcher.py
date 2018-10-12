b7e4b808019efa1d46f0150032a244f5403af93b
Unknown <fabiomercurio1995@gmail.com>
2018-10-12 19:50:58 +0200

    Matcher: added functions and performance.py

--- XMLmatcher.py
+++ XMLmatcher.py
@@ -5,7 +5,8 @@ from XMLparsing.XMLparser import *
 def list_creator(path_to_file):
     # type: (str) -> str
     """
-    Reads an XML-like file and returns the content of the text tag
+    Reads an XML-like file and returns a list of tuples
+    made of the text and the values in each TIMEX3 tag.
     :rtype: str
     :return: the text requested as a string
     :param path_to_file: name of the file to be red
@@ -26,15 +27,49 @@ def list_compare_strict(list1,list2):
                 cnt = cnt + 1
     return cnt
 
+def list_compare_relaxed(list1,list2):
+    cnt=0
+    to_not_be_seen_postitions_list1=[]
+    to_not_be_seen_postitions_list2=[]
+    for num1 in range(len(list1)):
+        for num2 in range(len(list2)):
+            if list1[num1]==list2[num2] and num1 not in to_not_be_seen_postitions_list1 and num2 not in to_not_be_seen_postitions_list2:
+                cnt = cnt+1
+                to_not_be_seen_postitions_list1.append(num1)
+                to_not_be_seen_postitions_list2.append(num2)
+    '''
+    RICORDATI DI FARE LA CORREZIONE PER CUI DEVI PRIMA SCEGLIERE IL MATCH PIU' GRANDE!!!!!!!!!!!
+    '''
+    for num1 in range(len(list1)):
+        for num2 in range(len(list2)):
+            if list1[num1][1]==list2[num2][1] and ((list1[num1][0] in list2[num2][0]) or (list2[num2][0] in list1[num1][0])) and num1 not in to_not_be_seen_postitions_list1 and num2 not in to_not_be_seen_postitions_list2:
+                cnt = cnt + 1
+                to_not_be_seen_postitions_list1.append(num1)
+                to_not_be_seen_postitions_list2.append(num2)
+    return cnt
+'''
 file1=list_creator('/home/fabio/Documenti/ComputationalLingiusticProjects/data/train/annotated/train_01.gold.tml')
 file2=list_creator('/home/fabio/Documenti/ComputationalLingiusticProjects/train_output.tml')
 
 #print file2
 print 'NUMBER OF EXPRESSIONS OF ORIGINAL GOLD STANDARD:'
-print length(file1)
+print len(file1)
 print 'NUMBER OF EXPRESSIONS OF OUR GOLD STANDARD:'
-print length(file2)
+print len(file2)
 print 'NUMBER OF MATCHED TIME EXPRESSIONS IS:'
-print list_compare_strict(file1,file2)
+print list_compare_relaxed(file1 , file2)
 #print file1
+'''
+
+def counter_matches(original_file_path, my_file_path):
+    '''Creating lists of elements from file path'''
+    or_file = list_creator(original_file_path)
+    my_file = list_creator(my_file_path)
+    '''Counting the number of expression on each file and the number of matches'''
+    nr_original_expressions = len(or_file)
+    nr_my_expressions = len(my_file)
+    nr_strict_matches = list_compare_strict(or_file,my_file)
+    nr_relaxed_matches = list_compare_relaxed(or_file,my_file)
+
+    return nr_original_expressions , nr_my_expressions , nr_strict_matches , nr_relaxed_matches
 
---
+++ performance.py
@@ -0,0 +1,19 @@
+from XMLmatcher import *
+
+def all_performances(original_gold_path, my_gold_path):
+
+    results = counter_matches(original_gold_path, my_gold_path)
+    #Computing evaluators
+    precision_strict = float(results[2])/results[1]
+    precision_relaxed = float(results[3])/results[1]
+    recall_strict = float(results[2])/results[0]
+    recall_relaxed = float(results[3])/results[0]
+    F1_strict = 2 * (precision_strict*recall_strict) / (precision_strict+recall_strict)
+    F1_relaxed = 2 * (precision_relaxed*recall_relaxed) / (precision_relaxed+recall_relaxed)
+
+    return precision_relaxed, recall_relaxed, F1_relaxed, F1_strict
+
+original_file = '/home/fabio/Documenti/ComputationalLingiusticProjects/data/train/annotated/train_01.gold.tml'
+my_file =  '/home/fabio/Documenti/ComputationalLingiusticProjects/train_output.tml'
+
+print all_performances( original_file, my_file)
