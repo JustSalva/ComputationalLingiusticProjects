2d18fee6e3346871a52b877510b2234e669685f7
Unknown <fabiomercurio1995@gmail.com>
2018-10-14 09:53:55 +0200

    Matcher: Bigger matching for list1 instead of list2

--- XMLmatcher.py
+++ XMLmatcher.py
@@ -20,7 +20,8 @@ def list_creator(path_to_file):
     return list1
 
 def list_compare_strict(list1,list2):
-    '''It takes the two string of expressions and counts how many matches strictly'''
+    '''It takes the two string of expressions and counts how many matches strictly.
+    list1 should correspond to the original gold standard file, list2 to our output file.'''
     cnt=0
     for elem1 in list1:
         for elem2 in list2:
@@ -29,7 +30,8 @@ def list_compare_strict(list1,list2):
     return cnt
 
 def list_compare_relaxed(list1,list2):
-    '''It takes the two string of expressions and counts how many matches relaxedly'''
+    '''It takes the two string of expressions and counts how many matches relaxedly.
+    list1 should correspond to the original gold standard file, list2 to our output file.'''
     cnt=0
     to_not_be_seen_positions_list1=[]
     to_not_be_seen_positions_list2=[]
@@ -43,12 +45,12 @@ def list_compare_relaxed(list1,list2):
     '''Now we check the positions of expressions. Overlap variable is used to check char_begin and 
     char_end. max_length is used to memorize which is the longest variable to match in my expressions,
     in order to match with maximal length.'''
-    for num1 in range(len(list1)):
-        begin_1 = int(list1[num1][2])
-        end_1 = int(list1[num1][3])
-        for num2 in range(len(list2)):
-            begin_2 = int(list2[num2][2])
-            end_2 = int(list2[num2][3])
+    for num2 in range(len(list2)):
+        begin_2 = int(list2[num2][2])
+        end_2 = int(list1[num2][3])
+        for num1 in range(len(list1)):
+            begin_1 = int(list1[num1][2])
+            end_1 = int(list1[num1][3])
             #overlap is used to know if there is overlapping between two expressions according to char_begin and char_end
             overlap = (begin_1<=begin_2 and end_1>=end_2) or (begin_2<=begin_1 and end_2>=end_1) or \
                       (begin_1<=begin_2<=end_1) or (begin_2<=begin_1<=end_2) or \
@@ -56,20 +58,20 @@ def list_compare_relaxed(list1,list2):
             max_length=0
             memo=None
             if list1[num1][1]==list2[num2][1] and overlap and num1 not in to_not_be_seen_positions_list1 and num2 not in to_not_be_seen_positions_list2:
-                current_length = len(list2[num2][0].split())
+                current_length = len(list1[num1][0].split())
                 max_length = max(max_length, current_length)
                 if max_length==current_length:
-                    memo = num2
+                    memo = num1
             if memo!=None:
                 cnt = cnt + 1
                 to_not_be_seen_positions_list1.append(num1)
                 to_not_be_seen_positions_list2.append(memo)
-                #print list1[num1],"MATCHES", list2[memo]
+                print list1[num1],"MATCHES", list2[memo]
     return cnt
 
-'''
-file1=list_creator('/home/fabio/Documenti/ComputationalLingiusticProjects/data/train/annotated/train_01.gold.tml')
-file2=list_creator('/home/fabio/Documenti/ComputationalLingiusticProjects/train_output.tml')
+
+file1=list_creator('/home/fabio/Documenti/ComputationalLingiusticProjects/data/train/annotated/train_02.gold.tml')
+file2=list_creator('/home/fabio/Documenti/ComputationalLingiusticProjects/train_02_output.tml')
 
 #print file2
 print 'NUMBER OF EXPRESSIONS OF ORIGINAL GOLD STANDARD:'
@@ -79,7 +81,7 @@ print len(file2)
 print 'NUMBER OF MATCHED TIME EXPRESSIONS IS:'
 print list_compare_relaxed(file1 , file2)
 #print file1
-'''
+
 
 def counter_matches(original_file_path, my_file_path):
     '''Creating lists of elements from file path'''
