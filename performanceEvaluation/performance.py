0f01e5ecb34ccddf335c8e5e971244d9f41470e9
Matteo Salvadore <matteo.salvadore@gmail.com>
2018-10-14 18:13:44 +0200

    like-latex print of performances

--- performanceEvaluation/performance.py
+++ performanceEvaluation/performance.py
@@ -12,8 +12,8 @@ def all_performances(original_gold_path, my_gold_path):
     F1_strict = 2 * (precision_strict*recall_strict) / (precision_strict+recall_strict)
     F1_relaxed = 2 * (precision_relaxed*recall_relaxed) / (precision_relaxed+recall_relaxed)
 
-    return precision_relaxed, recall_relaxed, F1_relaxed, F1_strict
+    return str(round(precision_relaxed,2))+ " & "+ str(round(recall_relaxed,2))+ " & "+ str(round(F1_relaxed,2))+ " & "+ str(round(F1_strict,2))
 
-original_file = 'data/test/annotated/test_01.gold.tml'
-my_file =  'result/test/test_01.output.tml'
+original_file = 'data/test/annotated/test_02.gold.tml'
+my_file =  'result/test/test_02.output.tml'
 print all_performances( original_file, my_file)
