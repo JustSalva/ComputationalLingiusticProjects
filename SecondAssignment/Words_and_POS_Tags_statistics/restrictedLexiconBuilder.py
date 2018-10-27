93c7b6a11ce8c3d36b4e02f8cd19a903f7051980
Matteo Salvadore <matteo.salvadore@gmail.com>
2018-10-27 11:48:24 +0200

    new test and train files + script to make them

--- SecondAssignment/Words_and_POS_Tags_statistics/restrictedLexiconBuilder.py
+++ SecondAssignment/Words_and_POS_Tags_statistics/restrictedLexiconBuilder.py
@@ -9,11 +9,16 @@ def isInTagSet(tag):
     return tag in possibleTags
 
 
+def splitWordAndToken(token):
+    word, tag = token.split('/', 1)
+    if not isInTagSet(tag):
+        word, tag = handleMultipleSlashes(token)
+    return word, tag
+
+
 def addToDictionary(token):
     try:
-        word, tag = token.split("/", 1)
-        if not isInTagSet(tag):
-            word, tag = handleMultipleSlashes(token)
+        word, tag = splitWordAndToken(token)
         if tag in dictionary:
             if word in wordFrequencies:
                 wordFrequencies[word] += 1
@@ -37,7 +42,7 @@ def addToDictionary(token):
                 wordFrequencies[word] = 1
     except Exception as e:
         print(e.with_traceback())
-        print("error!!!: " + token)
+        print('error!!!: ' + token)
         exit(-1)
 
 
@@ -46,37 +51,62 @@ def handleMultipleSlashes(token):
     found = False
     while not found:
         i += 1
-        split = token.split("/", i)
-        temp_word = "/".join(split[:i])
+        split = token.split('/', i)
+        temp_word = '/'.join(split[:i])
         temp_tag = split[i]
         if isInTagSet(temp_tag):
             found = True
     return temp_word, temp_tag
 
-fileScope = "train"
-with open('./../dataSets/dumas_' + fileScope + '_replaced', 'r') as dataset:
-    with open('./../dataSets/' + fileScope + '_dictionary', 'w') as tagDictionary:
 
-            for line in dataset:
-                # add every word to the dictionary (added only if its support reaches value 4)
-                for token in line.split():
-                    addToDictionary(token)
-                toPrint = ""
+def replaceWordIfNotFrequent(token):
+    word, tag = splitWordAndToken(token)
 
-            for key in dictionary:
-                toPrint = ""
-                for word in dictionary[key]:
-                    toPrint = toPrint + word + "(" + str(wordFrequencies[word]) + ")" + " "
+    if (word in wordFrequencies) and (wordFrequencies[word]) >= 4:
+        return token
 
-                toPrint = key + ":" + toPrint
-                #print(toPrint)
-                print(toPrint, file = tagDictionary)
+    return '<UNK>' + '/' + tag
 
-with open('./../dataSets/' + fileScope + '_frequencies', 'w') as frequencies:
+
+
+# build frequency and restricted lexicon tables
+with open('./../dataSets/dumas_train_replaced', 'r') as dataset:
+    with open('./../dataSets/train_dictionary', 'w') as tagDictionary:
+
+        for line in dataset:
+            # add every word to the dictionary (added only if its support reaches value 4)
+            for token in line.split():
+                addToDictionary(token)
+            toPrint = ''
+
+        for key in dictionary:
+            toPrint = ''
+            for word in dictionary[key]:
+                toPrint = toPrint + word + '(' + str(wordFrequencies[word]) + ')' + ' '
+
+            toPrint = key + ':' + toPrint
+            # print(toPrint)
+            print(toPrint, file=tagDictionary)
+
+with open('./../dataSets/train_frequencies', 'w') as frequencies:
     lexiconSize = 0
     for word in wordFrequencies:
-        print(word + " " + str(wordFrequencies[word]), file = frequencies)
+        print(word + ' ' + str(wordFrequencies[word]), file=frequencies)
         if wordFrequencies[word] >= 4:
             lexiconSize += 1
 
-print("lexicon size = " + str(lexiconSize))
+print('restricted lexicon size = ' + str(lexiconSize))
+
+# create new train and test files
+for fileScope in ['train', 'test']:
+    with open('./../dataSets/dumas_' + fileScope + '_replaced', 'r') as dataset:
+        with open('./../dataSets/' + fileScope, 'w') as finalDataset:
+            for line in dataset:
+                list = []
+                for token in line.split():
+                    list.append(replaceWordIfNotFrequent(token))
+                toPrint = ''
+                for i in range(0, len(list) - 1):
+                    toPrint = toPrint + list[i] + ' '
+                toPrint = toPrint + list[len(list) - 1]
+                print(toPrint, file=finalDataset)
